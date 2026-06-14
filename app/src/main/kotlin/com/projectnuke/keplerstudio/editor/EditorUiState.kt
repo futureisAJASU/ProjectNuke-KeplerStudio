dc063a802185e6316a24a097313a83ba2293e29c
AJASU <junyoung090928@gmail.com>
2026-06-15 00:12:04 +0900

    Store original preview bitmap

--- app/src/main/kotlin/com/projectnuke/keplerstudio/editor/EditorUiState.kt
+++ app/src/main/kotlin/com/projectnuke/keplerstudio/editor/EditorUiState.kt
@@ -5,6 +5,7 @@ import android.graphics.Bitmap
 data class EditorUiState(
     val isBusy: Boolean = false,
     val sourcePath: String? = null,
+    val originalPreviewBitmap: Bitmap? = null,
     val previewBitmap: Bitmap? = null,
     val params: EditParams = EditParams(),
     val viewport: ViewportState = ViewportState(),
