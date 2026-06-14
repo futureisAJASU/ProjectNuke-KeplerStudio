18efbbc299efdf966dc4648afd7cb1d7c69d185c
AJASU <junyoung090928@gmail.com>
2026-06-15 00:26:06 +0900

    Remove invalid pointer event import

--- app/src/main/kotlin/com/projectnuke/keplerstudio/ui/EditorScreen.kt
+++ app/src/main/kotlin/com/projectnuke/keplerstudio/ui/EditorScreen.kt
@@ -44,7 +44,6 @@ import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.asImageBitmap
 import androidx.compose.ui.graphics.graphicsLayer
-import androidx.compose.ui.input.pointer.awaitPointerEventScope
 import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.layout.onSizeChanged
