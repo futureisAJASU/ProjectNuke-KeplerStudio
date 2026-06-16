762964d7b416c3c042c80af3bf71f5fce5ac0c1c
AJASU <junyoung090928@gmail.com>
2026-06-17 01:23:20 +0900

    Raise minSdk for HEIF writer support

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,7 +9,7 @@ android {
 
     defaultConfig {
         applicationId = "com.projectnuke.keplerstudio"
-        minSdk = 26
+        minSdk = 28
         targetSdk = 36
         versionCode = 1
         versionName = "0.1.0"
