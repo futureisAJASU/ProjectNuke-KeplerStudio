dfc2671916aa45af79db406203df5bda62c7016e
AJASU <junyoung090928@gmail.com>
2026-06-17 22:07:28 +0900

    Keep active preset look for render passes

--- app/src/main/kotlin/com/projectnuke/keplerstudio/editor/PresetLookHandoff.kt
+++ app/src/main/kotlin/com/projectnuke/keplerstudio/editor/PresetLookHandoff.kt
@@ -2,18 +2,22 @@ package com.projectnuke.keplerstudio.editor
 
 object PresetLookHandoff {
     private var pendingLook: PresetColorLook? = null
+    private var activeLook: PresetColorLook? = null
 
     fun offer(look: PresetColorLook?) {
         pendingLook = look
+        if (look == null) activeLook = null
     }
 
-    fun consumeOrKeep(current: PresetColorLook?): PresetColorLook? {
+    fun consumeActive(): PresetColorLook? {
         val offered = pendingLook
         pendingLook = null
-        return offered ?: current
+        if (offered != null) activeLook = offered
+        return activeLook
     }
 
     fun clear() {
         pendingLook = null
+        activeLook = null
     }
 }
