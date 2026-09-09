e3d255b099e970d8ff556f86c9c11c1e081c9df9
futureisAJASU <junyoung090928@gmail.com>
2026-09-09 20:46:05 +0900

    test(android): stabilize android 16 test infrastructure

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -107,8 +107,9 @@ dependencies {
     testImplementation("io.mockk:mockk:1.13.17")
     testImplementation("org.robolectric:robolectric:4.16.1")
     testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test")
-    androidTestImplementation("androidx.test.ext:junit:1.2.1")
-    androidTestImplementation("androidx.test:runner:1.6.1")
+    androidTestImplementation("androidx.test.ext:junit:1.3.0")
+    androidTestImplementation("androidx.test:runner:1.7.0")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.7.0")
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
 }
 
